9aff5ff3055b3212b912dc14baede162ab07f720
Aleksandrs Kuskoveckis-Danilovs <52258392+kuskoveckis@users.noreply.github.com>
2021-01-28 20:37:43 +0000

    added overlay

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 class Weather {
   constructor() {
     this.weatherCards = document.querySelector(".weather-cards");
+    this.overlayContainer = document.querySelector(".overlay");
+    this.key = "3f86465e08723a0031ac7013675e093a";
     this.default = "London";
     this.city;
     this.day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -8,66 +10,38 @@ class Weather {
   }
 
   async fetchData(input) {
-    const key = "";
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${key}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${this.key}`);
     const data = await response.json();
     console.log(data);
     return data;
   }
 
   async fetchLocation(input) {
-    const key = "";
-
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${key}`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${this.key}`);
     const coordinates = await response.json();
     return coordinates;
   }
 
   async fetchOneCallApi(coord) {
-    const key = "";
-
     const { lat, lon } = coord[0];
-
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=${this.key}`
     );
     const data = await response.json();
     console.log(data);
     return data;
   }
 
-  generateOneCallCard(data) {
-    this.weatherCards.innerHTML = `
-        <article class="weather-container">
-          <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="">
-          <h1>${data.timezone}</h1>
-          <div class="date">Tuesday, January 10, 2021</div>
-          <div class="temperature">${data.current.temp}&deg;C</div>
-          <div class="underline"></div>
-          <div class="conditions">${data.current.weather[0].main}</div>
-          <div class="min-max-temp">25&deg;c / 23&deg;c</div>
-          <div class="daily-forecast">
-            <div class="daily">
-              <img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png" alt="">
-              <div class="daily-temp">${data.daily[0].temp.min}&deg;c / ${data.daily[0].temp.max}&deg;c </div>
-              <div class='weekday'>monday</div>
+  generateCardOverlay(data) {
+    this.overlayContainer.innerHTML = `
+      <div class="daily-forecast">
+                <div class="daily">
+                  <img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png" alt="">
+                  <div class="daily-temp">${data.daily[0].temp.min}&deg;c / ${data.daily[0].temp.max}&deg;c </div>
+                  <div class='weekday'>monday</div>
+                </div>
             </div>
-            <div class="daily">
-              <img src="http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png" alt="">
-              <div class="daily-temp">${data.daily[1].temp.min}&deg;c / ${data.daily[1].temp.max}&deg;c </div>
-              <div class='weekday'>monday</div>
-            </div>
-            <div class="daily">
-              <img src="http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png" alt="">
-              <div class="daily-temp">${data.daily[2].temp.min}&deg;c / ${data.daily[2].temp.max}&deg;c </div>
-              <div class='weekday'>monday</div>
-            </div>
-            
-          </div>
-          <div class="map-btn">^</div>
-        </article>
-      `;
+    `;
   }
 
   generateCard(data) {
@@ -87,6 +61,7 @@ class Weather {
     const sunsetMinutes = sunsetDate.getMinutes();
 
     this.weatherCards.innerHTML = `
+      <div class="container">
         <article class="weather-container">
           <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
           <h1>${data.name}</h1>
@@ -113,15 +88,78 @@ class Weather {
             <i class="fas fa-chevron-up"></i>
           </div>
         </article>
+        <article class="overlay"></article>
+      </div>
       `;
-  }
 
-  generateCardOverlay() {
-    this.weatherCards.innerHTML = `
-      <article class="overlay">
-        <h1>test jbfsfs</h1>
-      </article>
+    const overlay = document.querySelector(".overlay");
+    const main = document.querySelector(".weather-container");
+    const overlayBtn = document.querySelector(".daily-btn");
+    overlayBtn.addEventListener("click", () => {
+      this.fetchLocation(data.name)
+        .then((coord) => this.fetchOneCallApi(coord))
+        .then((data) => {
+          const over = document.querySelector(".overlay");
+          over.innerHTML = `
+          
+          <h2>Hourly</h2>
+            <div class='hourly'>
+              <div class='hour'>
+                <p class='time'>10:00</p>
+                <p>${Math.round(data.hourly[0].temp)}&deg;c</p>
+                <img src="http://openweathermap.org/img/wn/${data.hourly[0].weather[0].icon}@2x.png" alt="">
+              </div>
+              <div class='hour'>
+                <p class='time'>10:00</p>
+                <p>${Math.round(data.hourly[1].temp)}&deg;c</p>
+                <img src="http://openweathermap.org/img/wn/${data.hourly[0].weather[0].icon}@2x.png" alt="">
+              </div>
+              <div class='hour'>
+                <p class='time'>10:00</p>
+                <p>${Math.round(data.hourly[3].temp)}&deg;c</p>
+                <img src="http://openweathermap.org/img/wn/${data.hourly[0].weather[0].icon}@2x.png" alt="">
+              </div>
+              <div class='hour'>
+                <p class='time'>10:00</p>
+                <p>${Math.round(data.hourly[4].temp)}&deg;c</p>
+                <img src="http://openweathermap.org/img/wn/${data.hourly[0].weather[0].icon}@2x.png" alt="">
+              </div>
+            </div>
+            <h2>Daily</h2>
+            <div class="daily-forecast">
+                <div class="daily">
+                  <img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png" alt="">
+                  <div class="daily-temp">${Math.round(data.daily[0].temp.min)}&deg;c / ${Math.round(data.daily[0].temp.max)}&deg;c </div>
+                  <div class='weekday'>${this.day[currentDay]}</div>
+                </div>
+                <div class="daily">
+                  <img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png" alt="">
+                  <div class="daily-temp">${Math.round(data.daily[1].temp.min)}&deg;c / ${Math.round(data.daily[1].temp.max)}&deg;c </div>
+                  <div class='weekday'>${this.day[currentDay + 1]}</div>
+                </div>
+                <div class="daily">
+                  <img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png" alt="">
+                  <div class="daily-temp">${Math.round(data.daily[2].temp.min)}&deg;c / ${Math.round(data.daily[2].temp.max)}&deg;c </div>
+                  <div class='weekday'>${this.day[currentDay + 2]}</div>
+                </div>
+            </div>
+          <div class="weather-btn">
+            <i class="fas fa-chevron-down"></i>
+          </div>
+         
     `;
+
+          const weatherBtn = document.querySelector(".weather-btn");
+          weatherBtn.addEventListener("click", () => {
+            overlay.style.opacity = "0";
+            setTimeout(() => (overlay.style.height = "0"), 50);
+            setTimeout(() => (main.style.opacity = "1"), 100);
+          });
+        });
+      overlay.style.height = "100%";
+      setTimeout(() => (overlay.style.opacity = "1"), 130);
+      main.style.opacity = "0";
+    });
   }
 
   saveToLS(data) {
